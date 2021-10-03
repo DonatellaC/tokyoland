12e17fa8bad52017b337a6c424da7327b54ca264
DonatellaC <donatella.carta@mailbox.org>
2021-10-03 16:21:32 +0200

    create: homepage

---
+++ src/data.js
@@ -0,0 +1,35 @@
+import park from "./images/ueno-park.jpg";
+import museum from "./images/yayoi-kusama-museum.jpg";
+import teamLab from "./images/teamLab.jpg";
+import shibuyaCrossing from "./images/shibuya-crossing.jpg";
+
+export const data = [
+    {
+        id: 1,
+        name: "Ueno Park",
+        img: park,
+        description:
+            "This large park is a favorite destination of Tokyo residents, and within can be found many of the city's main attractions including the Tokyo National Museum, Ueno Zoo and the National Museum of Western Art.",
+    },
+    {
+        id: 2,
+        name: "Yayoi Kusama Museum",
+        img: museum,
+        description:
+            "The Yayoi Kusama Museum is a contemporary art museum in Tokyo, Japan dedicated to the work of the Japanese artist Yayoi Kusama. The Museum is located in the Shinjuku Ward, in the western suburbs of Japan. ",
+    },
+    {
+        id: 3,
+        name: "teamLab Borderless",
+        img: teamLab,
+        description:
+            "The interactive installations consist of constantly morphing patterns and designs that seem to flow seamlessly from room to room in a maze covering 10,000 square meters. ",
+    },
+    {
+        id: 4,
+        name: "Shibuya Crossing",
+        img: shibuyaCrossing,
+        description:
+            "Get hypnotized by the world's busiest intersection. It is estimated that between one and three thousand people crowd the crossing on an average signal change, and anywhere from a quarter to a half million people make the run every day.",
+    },
+];
