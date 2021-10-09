983427d83f0f03e4ac9a9f2bb199768b75cad8f0
DonatellaC <donatella.carta@mailbox.org>
2021-10-09 18:25:21 +0200

    create: Footer component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faTwitter,
-  faInstagram,
-  faFacebook,
-} from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { Card, Button, CardDeck, Image } from "react-bootstrap";
-
+import { Card, Button, CardDeck } from "react-bootstrap";
 import { data } from "./data";
 import Header from "./components/Header/Header";
 import TopSection from "./components/TopSection/TopSection";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const [places, setPlaces] = useState(data);
-  console.log(places);
   return (
     <div>
       <Header />
@@ -47,102 +39,7 @@ function App() {
           })}
         </CardDeck>
       </section>
-
-      <footer className="footer " id="footer">
-        <div className="justify-content-between">
-          <div className="row">
-            <div className="col-sm-12 col-md-6">
-              <h6 className="title-links">About</h6>
-              <p className="text-justify">
-                TokyoLand is a webpage to collect ideas for your trip to Japan.
-              </p>
-              <Button variant="danger">Contact</Button>
-            </div>
-
-            <div className="col-xs-6 col-md-3 ">
-              <h6 className="title-links">Categories</h6>
-              <ul className="footer-links">
-                <li>
-                  <a href="#things-to-do">Things to do</a>
-                </li>
-                <li>
-                  <a href="#">Culture</a>
-                </li>
-                <li>
-                  <a href="#">Food</a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-xs-6 col-md-3 ">
-              <h6 className="title-links">Links</h6>
-              <ul className="footer-links">
-
-                <li>
-                  <a href="#">Contact Us</a>
-                </li>
-                <li>
-                  <a href="#">Contribute</a>
-                </li>
-                <li>
-                  <a href="#">Team</a>
-                </li>
-                <li>
-                  <a href="#">Privacy Policy</a>
-                </li>
-                <li>
-                  <a href="#">Sitemap</a>
-                </li>
-              </ul>
-            </div>
-            <div className="container ">
-              <div>
-                <div className="row icons-container">
-                  <ul className="social-icons">
-                    <li >
-                      <a className="facebook" href="#">
-                        <i>
-                          {<FontAwesomeIcon icon={faFacebook} />}
-                        </i>
-                      </a>
-                    </li>
-                    <li>
-                      <a className="twitter" href="#">
-                        <i>
-                          {<FontAwesomeIcon icon={faTwitter} />}
-                        </i>
-                      </a>
-                    </li>
-                    <li>
-                      <a className="instagram" href="#">
-                        <i>
-                          {<FontAwesomeIcon icon={faInstagram} />}
-                        </i>
-                      </a>
-                    </li>
-                    <li>
-                      <a className="envelope" href="#">
-                        <i>
-                          {<FontAwesomeIcon icon={faEnvelope} />}
-                        </i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div>
-                <div className="row copyright-row">
-                  <p className="copyright-text">
-                    &copy; {new Date().getFullYear()} - TokyoLand. All rights
-                    reserved.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </footer>
+      <Footer />
     </div>
   );
 }
