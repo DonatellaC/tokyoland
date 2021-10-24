b109632e26430ca8475c545b739263a6e4cf83c7
DonatellaC <donatella.carta@mailbox.org>
2021-10-24 23:13:23 +0200

    updated food component

--- src/screens/Food/Food.js
+++ src/screens/Food/Food.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./Food.css";
+import lists from "./data";
 import Header from "../../components/Header/Header";
 
 function Food() {
@@ -6,6 +8,18 @@ function Food() {
     <div>
       <Header />
       <h1>Japanese Food</h1>
+      {lists.map((list) => {
+        const { id, name, description, image } = list;
+        return (
+          <div key={id} className="food">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{description}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/screens/Food/data.js
@@ -0,0 +1,40 @@
+
+const data = [
+    {
+        id: 1,
+        name: 'Nigiri',
+        description: "Small rice balls shaped by hand and topped with fish.",
+        image:
+            'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.qaNMN9pPBNvc3Iz-bxFAXAHaEz%26pid%3DApi&f=1',
+    },
+    {
+        id: 2,
+        name: 'Gunkan',
+        description: "Small cups made of sushi rice and dried seaweed filled with different toppings.",
+        image:
+            'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.PffNEGgOEey5QlvrjTmiFAHaFj%26pid%3DApi&f=1',
+    },
+    {
+        id: 3,
+        name: 'Norimaki',
+        description: "Sushi rice and multiple fillings, rolled in dried seaweed sheets.",
+        image:
+            'https://img.theculturetrip.com/1440x/smart/wp-content/uploads/2017/10/1024px-makizushi.jpg',
+    },
+    {
+        id: 4,
+        name: 'Temaki',
+        description: "Cones made of nori seaweed and filled with sushi rice, seafood and vegetables.",
+        image:
+            'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.DelLPC0RuSi6ojzrfXsWVQHaE7%26pid%3DApi&f=1',
+    },
+    {
+        id: 5,
+        name: 'Futomaki',
+        description: "Mega-sized norimaki and can be filled with a wide variety of fillings.",
+        image:
+            'https://img.theculturetrip.com/1440x/smart/wp-content/uploads/2017/10/4570202702_6a0c964753_b.jpg',
+    },
+];
+
+export default data
