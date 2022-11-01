ced830d227a693ee1e1b12cc3a9843653b095995
DonatellaC <donatella.carta@mailbox.org>
2022-11-01 20:23:02 +0100

    updated style background top section

--- src/components/TopSection/TopSection.js
+++ src/components/TopSection/TopSection.js
@@ -1,17 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./TopSection.css";
-import { Image } from "react-bootstrap";
 import background from "../../images/background.png";
 
 function TopSection() {
   return (
     <div className="container-background">
-      <Image
-        className="img-fluid"
-        src={background}
-        alt="Meguro River, Matsuno, Japan"
-      />
+      <img src={background} alt="Meguro River, Matsuno, Japan"></img>
       <div className="centered">
         <h1>{<Link to="/tokyo">Discover Tokyo</Link>}</h1>
       </div>
